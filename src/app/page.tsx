4db5a903cce895ab7c0f1a6248894f898ce8840a
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2024-04-01 15:22:55 -0300

    Criando componente da linha com titulo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Footer from "@/components/Footer/page";
 import HeaderDeslogado from "@/components/HeaderDeslogado/page";
 import HeaderLogado from "@/components/HeaderLogado/page";
+import TituloLinha from "@/components/TituloLinha/page";
 
 export default function Home() {
   return (
     <main>
       <p>Olá Top Care</p>
       <HeaderLogado/>
-
+      <TituloLinha titulo="ssss"/>
       <Footer/>
     </main>
   );
