79b9c33dfc8548aecbe228be901e33df3ce7842a
Carlos Bolzanell <carlosbolzanell@gmail.com>
2024-04-01 14:45:48 -0300

    Arquivo Nextjs configurado para desenvolvimento

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,23 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primaria': '#B5A6F3',
+        'secundaria': '#C9E47B',
+        'terciaria': '#DFEAFF',
+        'preto': '#322828',
+        'branco': '#F5F5F5',
+        'cinza': '#BDBDBD',
+        'cinza-escuro': '#4F4F4F',
+      },
+      fontFamily: {
+        'poppins': ['Poppins'],
+        'averia': ['Averia-Sans-Libre', 'sans-serif'],
       },
     },
   },
