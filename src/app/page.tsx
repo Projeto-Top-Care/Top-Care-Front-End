e118ed0ad03b6cc1258647e14df554632b2a4d2d
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-02 20:38:48 -0300

    Corrigindo page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Pergunta from "@/components/Pergunta/page";
-
 export default function Home() {
   return (
-    <main  className='flex justify-center items-center h-screen'>
-      <Pergunta/>
+    <main>
     </main>
   )
 }
