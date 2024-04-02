b9f13f7a4b2977719d7af92d0f50a2c35cd000e7
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-02 19:53:52 -0300

    Criação do componente CategoriaPergunta

---
+++ src/components/CategoriaPergunta/page.tsx
@@ -0,0 +1,8 @@
+const CategoriaPergunta = () => {
+    return (
+        <div className="flex justify-center text-center items-center font-poppins bg-terciaria w-[18%] h-[12%] rounded-lg">
+            <p className="text-lg font-medium">Compras e Pedidos</p>
+        </div>
+    )
+}
+export default CategoriaPergunta;
