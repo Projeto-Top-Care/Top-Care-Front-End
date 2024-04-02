0170b3c5d888208ed83e4b98e25f46ea393746d1
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-02 14:59:51 -0300

    Component historico consulta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import BotaoPequeno from "@/components/BotaoPequeno/page";
 import Footer from "@/components/Footer/page";
 import HeaderDeslogado from "@/components/HeaderDeslogado/page";
 import HeaderLogado from "@/components/HeaderLogado/page";
+import HistoricoConsulta from "@/components/HistoricoConsulta/page";
 import TituloLinha from "@/components/TituloLinha/page";
 
 export default function Home() {
   return (
     <main>
-      <BotaoPequeno/>
+     
     </main>
   );
 }
