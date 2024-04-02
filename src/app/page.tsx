7031fdac96ec88c057cf314cc7f24259791e150e
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-02 16:18:23 -0300

    Componente serviços

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import Footer from "@/components/Footer/page";
 import HeaderDeslogado from "@/components/HeaderDeslogado/page";
 import HeaderLogado from "@/components/HeaderLogado/page";
 import HistoricoConsulta from "@/components/HistoricoConsulta/page";
+import Servicos from "@/components/Servicos/page";
 import TituloLinha from "@/components/TituloLinha/page";
 
 export default function Home() {
   return (
     <main>
-     
+     <Servicos/>
     </main>
   );
 }
