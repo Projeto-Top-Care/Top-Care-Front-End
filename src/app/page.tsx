3afb43fe88887e942dbb6e715397b6ab7bb0a752
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-02 16:25:38 -0300

    Component plano

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import Footer from "@/components/Footer/page";
 import HeaderDeslogado from "@/components/HeaderDeslogado/page";
 import HeaderLogado from "@/components/HeaderLogado/page";
 import HistoricoConsulta from "@/components/HistoricoConsulta/page";
+import Plano from "@/components/Planos/page";
 import Servicos from "@/components/Servicos/page";
 import TituloLinha from "@/components/TituloLinha/page";
 
 export default function Home() {
   return (
     <main>
-     <Servicos/>
+     <Plano/>
     </main>
   );
 }
