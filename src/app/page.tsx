21270a12a7a128d2b2f3899f8450f324e009df77
Carlos Bolzanell <carlosbolzanell@gmail.com>
2024-04-02 16:12:06 -0300

    Atualizando a dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import InputText from "@/components/InputText/InputText"
 export default function Home() {
   return (
     <main className="flex items-center justify-center mt-20">
-      <div className="w-72">
-        <InputText placeholder="Nome" id="name" type='text' />
-      </div>
     </main>
   )
 }
