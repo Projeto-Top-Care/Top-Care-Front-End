6d28c4a7698aababf5d599c45d1cd1963a13d72b
Carlos Bolzanell <carlosbolzanell@gmail.com>
2024-04-05 14:24:59 -0300

    Componente de botão medio com alugmas melhorias

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import CadastroPet from "@/components/Pop-up/CadastroPet/CadastroPet";
-
-export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center mt-10">
-      <CadastroPet />
-    </main>
-  )
-}
