97d17c826d2c58b02535c15643b50fb208dc8dd9
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-05 14:09:11 -0300

    Parametros plano

--- src/components/Planos/Planos.tsx
+++ src/components/Planos/Planos.tsx
@@ -1,9 +1,15 @@
-const Plano = () => {
+interface IPlano{
+    tipo: string,
+    descricao: string,
+    valor: number
+}
+
+const Plano = ({tipo, descricao, valor}:IPlano) => {
     return (
         <div className="shadow-lg text-center font-poppins flex justify-center items-center flex-col bg-terciaria p-10 w-[28%] rounded-lg ">
-            <p className="mb-8 text-2xl font-extrabold">Básico</p>
-            <p className="mb-8">Plano básico oferece os serviços padrões mas ainda com total conforto para seu pet</p>
-            <p className="mb-8 text-xl">R$ 22,90 p/mês</p>
+            <p className="mb-8 text-2xl font-extrabold">{tipo}</p>
+            <p className="mb-8">{descricao}</p>
+            <p className="mb-8 text-xl">R${valor} p/mês</p>
         </div>
     )
 }
