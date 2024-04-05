998c98f1ddd077c18e24eab20a4bc833657eb0d5
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-05 15:49:52 -0300

    Responsividade planos

--- src/components/Planos/Planos.tsx
+++ src/components/Planos/Planos.tsx
@@ -6,10 +6,10 @@ interface IPlano{
 
 const Plano = ({tipo, descricao, valor}:IPlano) => {
     return (
-        <div className="shadow-lg text-center font-poppins flex justify-center items-center flex-col bg-terciaria p-10 w-[28%] rounded-lg ">
-            <p className="mb-8 text-2xl font-extrabold">{tipo}</p>
-            <p className="mb-8">{descricao}</p>
-            <p className="mb-8 text-xl">R${valor} p/mês</p>
+        <div className="shadow-lg text-center font-poppins flex justify-center items-center flex-col bg-terciaria p-10 w-[38%] lg:w-[28%] rounded-lg ">
+            <p className="mb-[8%] text-xl font-extrabold lg:text-2xl">{tipo}</p>
+            <p className="mb-[8%] text-xs lg:text-lg">{descricao}</p>
+            <p className="mb-[8%] text-xs lg:text-xl">R${valor} p/mês</p>
         </div>
     )
 }
