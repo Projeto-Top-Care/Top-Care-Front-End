a4a8427fd143b711cc9856c2c7940cf48400005d
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2024-04-05 14:56:39 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Avaliacao from "@/components/Avaliacao/Avaliacao";
-
 export default function Home() {
   return (
     <main className="">
-      <Avaliacao 
-        nomeUsuario="Kaorliny daieply" 
-        fotoUsuario="assets/cachorro_serviços.png" 
-        avaliacaoUsuario="asasdasd asasdasd asasdasd asasdasd asasdasd asasdasd asasdasd asasdasd asasdasd asasdasd asasdasd asasdasdasasdasdasasdasdasasdasdasasdasd asasdasd asasdasd asasdasdasasdasd asasdasd"
-      />
+    
     </main>
   )
 }
