0d5cecdf98f635ed2025d131467839a1acfc1c04
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-05 16:17:05 -0300

    Parametros historico consulta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import HistoricoConsulta from "@/components/HistoricoConsulta/HistoricoConsulta";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center mt-10">
+      <HistoricoConsulta/>
     </main>
   )
 }
--- src/components/HistoricoConsulta/HistoricoConsulta.tsx
+++ src/components/HistoricoConsulta/HistoricoConsulta.tsx
@@ -1,21 +1,41 @@
-const HistoricoConsulta = () => {
+interface IHistoricoConsulta{
+    titulo:string,
+
+    data: string,
+    horario: string,
+    pet: string,
+    valor: string,
+    forma: string,
+    profissional: string
+
+    dataM: string,
+    horarioM: string,
+    petM: string,
+    valorM: number,
+    formaM: string,
+    profissionalM: string
+}   
+
+const HistoricoConsulta = ({titulo,data,horario,pet,valor,forma,profissional,dataM,horarioM,petM,valorM,formaM,profissionalM}:IHistoricoConsulta) => {
     return (
         <div className="font-poppins bg-branco shadow-lg rounded-lg">
-            <p className="text-2xl font-bold bg-secundaria text-center p-2 rounded-t-lg">Banho e tosa</p>
+            <p className="text-2xl font-bold bg-secundaria text-center p-2 rounded-t-lg">{titulo}</p>
             <div className="flex flex-row justify-around p-3">
-                <div className="font-bold">
-                    <p className="mb-8">Data</p>
-                    <p className="mb-8">Pet</p>
-                    <p className="mb-8">Valor</p>
-                    <p className="mb-8">Forma</p>
-                    <p className="">Profissional</p>
+                <div className="font-bold mr-10">
+                    <p className="mb-5">{data}</p>
+                    <p className="mb-5">{horario}</p>
+                    <p className="mb-5">{pet}</p>
+                    <p className="mb-5">{valor}</p>
+                    <p className="mb-5">{forma}</p>
+                    <p className="">{profissional}</p>
                 </div> 
                 <div className="text-right">
-                    <p className="mb-8">01/12/2023 ás 15:45h</p>
-                    <p className="mb-8">Nina</p>
-                    <p className="mb-8">120R$</p>
-                    <p className="mb-8">Crédito 1X</p>
-                    <p className="">Carla</p>
+                    <p className="mb-5">{dataM}</p>
+                    <p className="mb-5">{horarioM}</p>
+                    <p className="mb-5">{petM}</p>
+                    <p className="mb-5">R${valorM}</p>
+                    <p className="mb-5">{formaM}</p>
+                    <p className="">{profissionalM}</p>
                 </div> 
             </div>
         </div>
