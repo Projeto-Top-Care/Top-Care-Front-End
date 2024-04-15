2c3c357bbc98144a765da6ddd177c068a67da5af
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-15 16:54:04 -0300

    Responsivando componente loja

--- src/components/Lojas/Lojas.tsx
+++ src/components/Lojas/Lojas.tsx
@@ -12,22 +12,22 @@ interface ILoja{
 
 const Lojas = ({src, cidade, estado, endereco, contato, funcionamentoDias, funcionamentoHora}:ILoja) => {
     return (
-        <div className="font-poppins flex flex-col items-center w-[22%]">
-            <img className="rounded-t-lg" src={src} />
+        <div className="font-poppins flex flex-col items-center lg:w-[22%] md:w-[28%] w-[39%] ">
+            <img className=" rounded-t-lg" src={src} />
             <div className="flex justify-center border border-preto rounded-b-lg">
-                <div className="mt-[5%]">
+                <div className="mt-[5%] ">
                     <LuMapPin size={25} color="#B5A6F3"/>
                 </div>
                 <div className="mt-[5%] mb-[10%] w-[74%]">
-                    <p className="text-lg text-preto">Top Care {cidade} - {estado}</p>
-                    <div className="text-cinza-escuro">
-                        <p className="mt-[10%] font-bold ">Endereço</p>
-                        <p className="text-sm">{endereco}</p> 
-                        <p className="mt-[10%] font-bold">Contato</p>
-                        <p className="text-sm">{contato}</p>
-                        <p className="mt-[10%] font-bold">Funcionamento</p>
-                        <p className="text-sm">{funcionamentoDias}</p>    
-                        <p className="text-sm">{funcionamentoHora}</p>  
+                    <p className=" text-preto md:text-lg text-sm">Top Care {cidade} - {estado}</p>
+                    <div className="text-cinza-escuro md:bg-red">
+                        <p className="mt-[10%] font-bold md:text-base text-sm">Endereço</p>
+                        <p className="md:text-sm text-xs">{endereco}</p> 
+                        <p className="mt-[10%] font-bold md:text-base text-sm">Contato</p>
+                        <p className="md:text-sm text-xs">{contato}</p>
+                        <p className="mt-[10%] font-bold md:text-base text-sm">Funcionamento</p>
+                        <p className="md:text-sm text-xs">{funcionamentoDias}</p>    
+                        <p className="md:text-sm text-xs">{funcionamentoHora}</p>  
                     </div> 
                 </div>
             </div>
