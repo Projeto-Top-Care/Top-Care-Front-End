24a3217c86e25ff14cb05bc9cf274a8a2c18bf01
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-15 16:08:11 -0300

    lojas modificado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Lojas from "@/components/Lojas/Lojas";
 
 export default function Home() {
  
   return (
     <main className="flex flex-col items-center justify-center mt-10">
+      <Lojas src='./assets/Lojas1.png/' cidade="Jaraguá" estado="SC" endereco="R. Roberto Seidel, 732 - Sala 2 - João Tozini, Corupá - SC, 89278-000"
+      contato="+55 (47) 99999-9999" funcionamentoDias="Segunda a sábado" funcionamentoHora="8h as 20h"/>
+        
     </main>
   )
 }
--- src/components/Lojas/Lojas.tsx
+++ src/components/Lojas/Lojas.tsx
@@ -3,20 +3,32 @@ import { LuMapPin } from "react-icons/lu";
 interface ILoja{
     src: string,
     cidade: string,
-    rua: string
+    estado: string,
+    endereco: string,
+    contato: string,
+    funcionamentoDias: string,
+    funcionamentoHora: string
 }
 
-const Lojas = ({src, cidade, rua}:ILoja) => {
+const Lojas = ({src, cidade, estado, endereco, contato, funcionamentoDias, funcionamentoHora}:ILoja) => {
     return (
-        <div className="font-poppins flex flex-col gap-4 items-center">
+        <div className="font-poppins flex flex-col items-center w-[22%]">
             <img className="rounded-t-lg" src={src} />
-            <div className="flex gap-2 justify-center">
-                <div className="mt-2">
+            <div className="flex justify-center border border-preto rounded-b-lg">
+                <div className="mt-[5%]">
                     <LuMapPin size={25} color="#B5A6F3"/>
                 </div>
-                <div className="grid">
-                    <p className="text-lg">Top Care {cidade}</p>
-                    <p className="text-sm text-cinza-escuro">{rua}</p>
+                <div className="mt-[5%] mb-[10%] w-[74%]">
+                    <p className="text-lg text-preto">Top Care {cidade} - {estado}</p>
+                    <div className="text-cinza-escuro">
+                        <p className="mt-[10%] font-bold ">Endereço</p>
+                        <p className="text-sm">{endereco}</p> 
+                        <p className="mt-[10%] font-bold">Contato</p>
+                        <p className="text-sm">{contato}</p>
+                        <p className="mt-[10%] font-bold">Funcionamento</p>
+                        <p className="text-sm">{funcionamentoDias}</p>    
+                        <p className="text-sm">{funcionamentoHora}</p>  
+                    </div> 
                 </div>
             </div>
         </div>
