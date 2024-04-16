6680f6107fc3e777e84738c88308ba815cfa2afd
Karoliny Daiana Cieply <karolinyc@weg.net>
2024-04-15 21:49:30 -0300

    Resonsividade de alguns componentes/ Página inicial de apresentação

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,26 +6,26 @@ import { BsTwitterX } from "react-icons/bs";
 const Footer = () => {
     return (
         <div className="bg-secundaria font-poppins font-light">
-            <div className="px-[70px] py-[30px] flex flex-row justify-between items-start text-lg">
-                <div className="pr-8">
-                    <img className="h-[30%]" src="./assets/logo.png" />
+            <div className="px-[70px] py-[30px] justify-between grid md:grid-cols-5 grid-cols-4 gap-9 items-start text-lg">
+                <div className="md:w-[5rem] w-[4rem]">
+                    <img className="" src="./assets/logo.png" />
                 </div>
 
-                <div className="flex flex-col gap-1 text-base">
+                <div className="flex flex-col gap-1 text-sm md:text-base">
                     <h5>Empresa</h5>
-                    <a className="underline text-cinza-escuro text-sm">Sobre nós</a>
-                    <a className="underline text-cinza-escuro text-sm">Perguntas frequentes</a>
+                    <a className="underline text-cinza-escuro md:text-sm text-xs">Sobre nós</a>
+                    <a className="underline text-cinza-escuro md:text-sm text-xs">Perguntas frequentes</a>
                 </div>
 
-                <div className="flex flex-col gap-1 text-base">
+                <div className="flex flex-col gap-1 text-sm md:text-base">
                     <h5>Fale conosco</h5>
-                    <a className="underline text-cinza-escuro text-sm">Email</a>
-                    <a className="underline text-cinza-escuro text-sm">Telefone/ SMS</a>
+                    <a className="underline text-cinza-escuro md:text-sm text-xs">Email</a>
+                    <a className="underline text-cinza-escuro md:text-sm text-xs">Telefone/ SMS</a>
                 </div>
 
-                <div>
-                    <h5 className="text-base">Nossas redes sociais</h5>
-                    <div className="flex flex-row gap-2 pt-2 justify-center">
+                <div className="flex flex-col items-center">
+                    <h5 className="md:text-base text-sm">Nossas redes sociais</h5>
+                    <div className="flex flex-row gap-2 pt-2 ">
                         <FaInstagram style={ { color: "#322828", fontSize: "1.4rem" }}/>
                         <FaFacebookSquare style={ { color: "#322828", fontSize: "1.4rem" }}/>
                         <FaYoutube style={ { color: "#322828", fontSize: "1.4rem" }}/>
@@ -33,9 +33,9 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-2">
-                    <h5 className="text-base">Métodos de pagamento</h5>
-                    <img className="w-[190px]" src="assets/footer-pagamento.png" />
+                <div className="flex flex-col">
+                    <h5 className="md:text-base text-sm">Métodos de pagamento</h5>
+                    <img className="md:w-[190px]" src="assets/footer-pagamento.png" />
                 </div>
             </div>
 
