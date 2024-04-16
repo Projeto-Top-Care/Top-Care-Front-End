fa18d39a11b21a6f35e7f857044aea868b9144cb
Karoliny Daiana Cieply <karolinyc@weg.net>
2024-04-16 16:40:47 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import BarraPesquisa from "@/components/BarraPesquisa/BarraPesquisa";
-
 export default function Home() {
  
   return (
-    <main className="flex flex-col items-center justify-center mt-10"> 
-    <BarraPesquisa placeholder="Pesquisar"/>     
+    <main className="flex flex-col items-center justify-center mt-10">      
     </main>
   )
 }
