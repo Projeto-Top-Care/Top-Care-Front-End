85057fd40380365d23c5758c489b01f3edc04c17
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-16 14:27:48 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@
 export default function Home() {
  
   return (
+<<<<<<< HEAD
     <main className="flex flex-col items-center justify-center mt-10">
+=======
+    <main>
+>>>>>>> Feature/Pagina-Lojas
       
     </main>
   )
