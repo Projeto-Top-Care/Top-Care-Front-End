240a120a13390ab5f344b283032348fc9791d718
Karoliny Daiana Cieply <karolinyc@weg.net>
2024-04-17 21:54:42 -0300

    Responsividade do header deslogado

--- src/components/BarraPesquisa/BarraPesquisa.tsx
+++ src/components/BarraPesquisa/BarraPesquisa.tsx
@@ -6,11 +6,11 @@ interface IBarraPesquisa {
 
 export default function BarraPesquisa({placeholder}: IBarraPesquisa){
     return(
-        <div className="border border-cinza bg-branco flex rounded-lg h-8">
-            <div className="w-[10%] flex items-center justify-center">
+        <div className="border-[0.1rem] border-cinza bg-branco flex w-full rounded-lg h-8">
+            <div className="size-[2rem] flex items-center justify-center">
                 <button><FaSearch style={{color: "#322828"}}/></button>
             </div>
-            <div className="w-auto flex">
+            <div className="w-full flex items-center justify-start">
                 <input type="text" className="focus:outline-0 w-full rounded placeholder:text-cinza-escuro font-poppins" placeholder={placeholder}/>
             </div>
         </div>
