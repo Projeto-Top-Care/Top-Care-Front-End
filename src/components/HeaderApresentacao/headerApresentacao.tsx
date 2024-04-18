da1bc3ccdf8ad60d7a90717fbf21ada5460a104d
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-17 21:49:19 -0300

    sobre nos

--- src/components/HeaderApresentacao/headerApresentacao.tsx
+++ src/components/HeaderApresentacao/headerApresentacao.tsx
@@ -4,12 +4,13 @@ export default function HeaderApresentacao() {
     return (
         <div className="bg-primaria md:px-20 px-10 md:py-3 py-2 flex flex-row font-poppins justify-between items-center text-preto">
             <div className=''>
-                <img className="md:w-[64px] w-[40px] transition duration-200" src="assets/logo.png"/>
+            <a id="div_link" href="./paginaInicialApresentacao"><img className="md:w-[64px] w-[40px] transition duration-200" src="assets/logo.png"/></a>
             </div>
 
-            <div className="flex flex-row md:gap-24 gap-4 justify-center w-full">
-                <a className="hover:underline md:text-sm text-xs" href="#">Contato</a>
-                <a className="hover:underline md:text-sm text-xs" href="#">Lojas</a>
+            <div className="flex flex-row md:gap-24 gap-4 justify-center w-full font-poppins">
+                <a className="hover:underline md:text-sm text-xs" href="./contato">Contato</a>
+                <a className="hover:underline md:text-sm text-xs" href="./lojas">Lojas</a>
+                <a className="hover:underline md:text-sm text-xs" href="./lojas">Sobre nós</a>
             </div>
         </div>
     )
