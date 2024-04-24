03036729075fa6618954fb1ada3b820f77fc2de5
Carlos Bolzanell <carlos_santos30@estudante.sesisenai.org.br>
2024-04-24 20:49:05 -0300

    Atualizando TextArea

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -4,7 +4,7 @@ export default function TextArea(props: TextareaHTMLAttributes<HTMLTextAreaEleme
     return (
         <div className="relative h-full">
             <textarea {...props} className="peer font-poppins px-2 py-2 rounded-md w-full h-full text-sm text-cinza-escuro border-cinza border
-            focus:bg-branco bg-branco placeholder-transparent focus:outline-none"/>
+            focus:bg-branco bg-branco placeholder-transparent focus:outline-none resize-none"/>
 
             <label htmlFor={props?.id} className="absolute left-1.5 font-poppins px-1 lg:text-sm text-xs transition-all bg-branco rounded lg:-top-2.5 -top-2
             md:peer-placeholder-shown:text-sm peer-placeholder-shown:text-xs peer-placeholder-shown:text-cinza-escuro peer-placeholder-shown:top-2.5
