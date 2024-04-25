61585c1d17a5ef1043c1ae00f63d3b3fd3829070
Carlos Bolzanell <carlos_santos30@estudante.sesisenai.org.br>
2024-04-25 15:00:03 -0300

    Header modificado levemente

--- src/components/HeaderApresentacao/headerApresentacao.tsx
+++ src/components/HeaderApresentacao/headerApresentacao.tsx
@@ -19,7 +19,7 @@ export default function HeaderApresentacao() {
     return (
         <div className="bg-primaria md:px-20 px-10 md:py-3 py-2 flex md:flex-row flex-row-reverse font-poppins justify-between items-center text-preto">
             <div className=''>
-                <a id="div_link" href="./paginaInicialApresentacao"><img className="md:w-[64px] w-[40px] transition duration-200" src="assets/logo.png"/></a>
+                <a id="div_link" href="./"><img className="md:w-[64px] w-[40px] transition duration-200" src="assets/logo.png"/></a>
             </div>
 
             <div className="max-sm:hidden flex flex-row md:gap-24 gap-4 justify-center w-full font-poppins">
