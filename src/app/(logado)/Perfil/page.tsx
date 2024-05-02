9daf0e0e739bcbe87d6f96326977ac95cfc53acf
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-05-02 16:50:05 -0300

    continuando o perfil

--- src/app/(logado)/Perfil/page.tsx
+++ src/app/(logado)/Perfil/page.tsx
@@ -1,4 +1,7 @@
+import BotaoGrande from "@/components/BotaoGrande/BotaoGrande";
+import BotaoPequeno from "@/components/BotaoPequeno";
 import CartoesSalvos from "@/components/CartoesSalvos/CartoesSalvos";
+import Endereco from "@/components/Endereço/Endereco";
 import InputEstatico from "@/components/InputEstatico/InputEstatico";
 import PerfilFoto from "@/components/PerfilFoto/PerfilFoto";
 import TituloLinha from "@/components/TituloLinha/TituloLinha";
@@ -6,46 +9,55 @@ import TituloLinha from "@/components/TituloLinha/TituloLinha";
 export default function Perfil() {
     return (
         <main className="bg-branco text-preto">
-            <section>
+            <section className="mt-6">
                 <TituloLinha titulo="Minha conta" />
                 <div className="mt-10 md:ml-44 ml-6">
                     <PerfilFoto src="./assets/cachorro-perfil.png/" nome="Kristian" />
                 </div>
             </section>
             <section className="rounded-xl bg-terciaria mx-44 mt-12">
-                <div className="flex justify-center px-12 pb-7">
-                    <div className="w-80">
+                <div className="flex justify-around px-7 pb-7">
+                    <div className="min-w-80">
                         <InputEstatico titulo="Nome completo" info="Kristian Erdmann" />
                         <InputEstatico titulo="Senha" info="**********" />
                         <InputEstatico titulo="Sexo" info="Masculino" />
                     </div>
                     <div className="">
-                        <div className="ml-24 ">
+                        <div className="">
                             <div className="flex">
-                            <div className="w-80"><InputEstatico titulo="Email" info="kristian@gmail.com" /></div>
-                            <div className="mr-4 ml-7"><InputEstatico titulo="DDD +" info="047" /></div>
-                            <div className="w-36"><InputEstatico titulo="Celular" info="99999-9999" /></div>
+                                <div className="w-80"><InputEstatico titulo="Email" info="kristian@gmail.com" /></div>
+                                <div className="mr-4 ml-7"><InputEstatico titulo="DDD +" info="047" /></div>
+                                <div className="w-36"><InputEstatico titulo="Celular" info="99999-9999" /></div>
                             </div>
                             <div className="flex">
-                            <div className="w-80"><InputEstatico titulo="CPF" info="999.999.999-99" /></div>
-                            <div className="ml-7 w-52"><InputEstatico titulo="Data de Nascimento" info="99/99/9999" /></div>
+                                <div className="w-80"><InputEstatico titulo="CPF" info="999.999.999-99" /></div>
+                                <div className="ml-7 w-52"><InputEstatico titulo="Data de Nascimento" info="99/99/9999" /></div>
                             </div>
                         </div>
-                        <div className="mt-6 ml-24 font-poppins">Cartões Salvos<CartoesSalvos tipoCartao="crédito" nome="Kristian Erdmann" dataValidade="12/28" finalCartao={9875} /></div>
+                        <div className="mt-6 font-poppins">Cartões Salvos<CartoesSalvos tipoCartao="crédito" nome="Kristian Erdmann" dataValidade="12/28" finalCartao={9875} /></div>
                     </div>
                 </div>
             </section>
+            <section className="mt-8">
+                <TituloLinha titulo="Endereços" />
+            </section>
+            <section className="grid place-content-center">
+                <div className="grid gap-11 mt-8 mb-12 md:grid-cols-3 grid-cols-1">
+                    <div><Endereco titulo="Casa" cep="99999-999" estado="Santa Catarina" bairro="Centro" rua="Sen. Luiz Henrique da Silveira" numero={999} complemento="Apto 300" /></div>
+                    <div><Endereco titulo="Casa" cep="99999-999" estado="Santa Catarina" bairro="Centro" rua="Sen. Luiz Henrique da Silveira" numero={999} complemento="Apto 300" /></div>
+                    <div><Endereco titulo="Casa" cep="99999-999" estado="Santa Catarina" bairro="Centro" rua="Sen. Luiz Henrique da Silveira" numero={999} complemento="Apto 300" /></div>
+                </div>
+                <div className="w-56 justify-self-center">
+                    <BotaoGrande title="Adicionar Endereço" background={"bg-secundaria"} type={"button"} />
+                </div>
+            </section>
+            <section className="mt-8">
+                <TituloLinha titulo="Endereços" />
+            </section>
+
+
         </main>
     )
 }
 
-{/* <div className="ml-9">
-                            <div className="flex">
-                                <div className="mr-4"><InputEstatico titulo="DDD +" info="047" /></div>
-                                <InputEstatico titulo="Celular" info="99999-9999" />
-                            </div>
-                            <div><InputEstatico titulo="Data" info="99/99/9999" /></div>
-                            </div> */}
 
-
-//<div className=""><CartoesSalvos tipoCartao="crédito" nome="Kristian Erdmann" dataValidade="12/28" finalCartao={9875}/></div>
