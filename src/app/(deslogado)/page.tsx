ff673c8df34eedf633afae8d55467b3c93f5b25f
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2024-05-09 21:53:08 -0300

    a

--- src/app/(deslogado)/page.tsx
+++ src/app/(deslogado)/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import Acordeao from "@/components/Acordeao/acordeao";
-import ItemAcordiao from "@/components/ItemAcordeao/itemAcordeao";
 import PaginaInicial from "./paginaInicial/page";
 
 export default function Home() {
