9cc143ce1d64467b2e19c39eced97833b1ce9624
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2024-05-13 16:20:55 -0300

    Componente do resumo do pedido

--- src/app/(logado)/pagamentoBoleto/page.tsx
+++ src/app/(logado)/pagamentoBoleto/page.tsx
@@ -3,10 +3,15 @@ import BotaoGrande from "@/components/BotaoGrande/BotaoGrande"
 import ResumoPedido from "@/components/ResumoPedido/resumoPedido"
 
 export default function pagamentoBoleto() {
+
+    const produtos = ["Kit para banho Sanol Cachorros e Gatos", "Pote para ração em formato de pata de cachorro", "Coleira com pingente tamanho M unissex para cães e gatos"]
+    const quantidades = [1, 1, 1]
+    const precos = [11.1, 11.1, 11.1]
+
     return (
         <main>
             <section className="p-4">
-                <ResumoPedido />
+                <ResumoPedido produtos={produtos} quantidades={quantidades} precos={precos} desconto={9} frete={0} />
             </section>
         </main>
     )
--- src/components/ResumoPedido/resumoPedido.tsx
+++ src/components/ResumoPedido/resumoPedido.tsx
@@ -1,53 +1,60 @@
-export default function ResumoPedido() {
+'use client'
+import { useState } from "react"
+
+interface IResumoPedido{
+    produtos: string[],
+    quantidades: number[],
+    precos: number[],
+    desconto: number,
+    frete: number
+}
+
+export default function ResumoPedido({produtos, quantidades, precos, desconto, frete}:IResumoPedido) {
+
+    const [subtotal, setSubtotal] = useState()
+
+    const [total, setTotal] = useState(0)
+
     return (
         <main>
             <div className="text-preto font-poppins w-full">
-                <h2 className="font-bold text-lg pb-2">Resumo geral</h2>
+                <h2 className="font-bold text-base sm:text-lg pb-2">Resumo geral</h2>
 
                 <div className="border-cinza border-[1px] rounded-lg p-4">
-                    <h4 className="font-medium text-base">Produtos</h4>
+                    <h4 className="font-medium text-sm sm:text-base">Produtos</h4>
 
-                    <div className="flex flex-col text-sm py-4">
-                        <div className="flex flex-row justify-between sm:gap-8 gap-2">
-                            <p>1x</p>
-                            <p className="w-full text-start line-clamp-1">Kit para banho Sanol Cachorros e Gatos</p>
-                            <p>R$11,10</p>                            
+                    {
+                        <div className="flex flex-col text-sm py-4">
+                            {produtos.map((item, index) => (
+                                <div className="flex flex-row justify-between sm:gap-8 gap-2" key={index}>
+                                    <p className="text-xs sm:text-sm">{quantidades[index]}x</p>
+                                    <p className="w-full text-start line-clamp-1 text-xs sm:text-sm">{item}</p>
+                                    <p className="text-xs sm:text-sm">R${precos[index]}</p> 
+                                </div>
+                            ))}
                         </div>
-
-                        <div className="flex flex-row justify-between sm:gap-8 gap-2">
-                            <p>1x</p>
-                            <p className="w-full text-start line-clamp-1">Pote para ração em formato de pata de cachorro 200ml</p>
-                            <p>R$11,10</p> 
-                        </div>
-
-                        <div className="flex flex-row justify-between sm:gap-8 gap-2">
-                            <p>1x</p>
-                            <p className="w-full text-start line-clamp-1">Coleira com pingente tamanho M unissex para cachorro e gato</p>
-                            <p>R$11,10</p>
-                        </div>
-
-                    </div>
+                    }
                     
                     <div className="flex flex-col border-t-[1px] border-cinza py-4">
                         <div className="flex flex-row justify-between">
-                            <p className="font-medium text-base">Subtotal</p>
-                            <p className="text-sm">R$47,00</p>
+                            <p className="font-medium text-sm sm:text-base">Subtotal</p>
+                            <p className="text-xs sm:text-sm">R$47,00</p>
                         </div>
 
                         <div className="flex flex-row justify-between">
-                            <p className="font-medium text-base">Desconto</p>
-                            <p className="text-sm">R$9,00</p>                            
+                            <p className="font-medium text-sm sm:text-base">Desconto</p>
+                            <p className="text-xs sm:text-sm">R${desconto}</p>                            
                         </div>
 
                         <div className="flex flex-row justify-between">
-                            <p className="font-medium text-base">Frete</p>
-                            <p className="text-sm">Grátis</p>                            
+                            <p className="font-medium text-sm sm:text-base">Frete</p>
+                            <p className="text-xs sm:text-sm">{frete}</p>                            
                         </div>
                     </div>
 
                     <div className="flex flex-row justify-between border-t-[1px] border-cinza pt-4">
-                        <p className="font-medium text-base">Valor total</p>
-                        <p className="text-sm">R$38,30</p>
+                        <p className="font-medium text-sm sm:text-base">Valor total</p>
+                        <p className="text-xs sm:text-sm">R$38,30</p>
                     </div>
 
                 </div>                
