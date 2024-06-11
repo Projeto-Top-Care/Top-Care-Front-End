313bc08935de773f8dd0938dbee09ffac6cca459
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-06-10 21:10:20 -0300

    a

---
+++ src/types/pedidos.ts
@@ -0,0 +1,12 @@
+export interface Pedidos {
+    id: number;
+    Cod_pedido: string;
+    Dt_pedido: string;
+    Produto: string;
+    Quantidade: number;
+    Cliente: string;
+    Destino: string;
+    Valor: string;
+    Status: string;
+    Pagamento: string;
+  };
