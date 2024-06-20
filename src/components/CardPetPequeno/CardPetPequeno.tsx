1a4d61a50f604e16d463c9085011cfe7528600c8
LuanaBecker24 <130504315+LuanaBecker24@users.noreply.github.com>
2024-06-20 16:25:31 -0300

    Arrumando o card do pet

--- src/components/CardPetPequeno/CardPetPequeno.tsx
+++ src/components/CardPetPequeno/CardPetPequeno.tsx
@@ -5,20 +5,20 @@ type pettype = {
     nomePet: string
     tipoAnimal:string
     racaPet: string
-    idadePet: number
+    porte: string
 }
 
-const CardPetPequeno = ({tipoAnimal, fotoPet, nomePet, racaPet, idadePet}: pettype) => {
+const CardPetPequeno = ({tipoAnimal, fotoPet, nomePet, racaPet, porte}: pettype) => {
     return (
-        <div className="flex bg-branco shadow-2xl py-4 justify-center gap-4 rounded-lg w-[290px]">
-            <div>
-                <img className='rounded-full' src={fotoPet} />
+        <div className="mt-4 flex bg-branco border border-cinza justify-center items-center lg:gap-5 gap-4 rounded-lg lg:w-[22%] w-full h-36 p-2">
+            <div className="lg:w-[40%] w-[50%]">
+                <img className='lg:ml-3 md:ml-8 ml-2 rounded-full lg:w-full md:w-[70%] sm:w-full' src={fotoPet} />
             </div>
-            <div className="text-sm text-preto font-poppins">
-                <p className="mb-4 lg:text-lg ">{nomePet}</p>
+            <div className="text-sm text-preto font-poppins lg:w-[60%] md:w-[50%] w-full mr-2 ml-1">
+                <p className="gap-1 lg:text-lg">{nomePet}</p>
                 <p>Animal: {tipoAnimal}</p>
                 <p>Raça: {racaPet}</p>
-                <p>Idade: {idadePet} anos</p>
+                <p>Porte: {porte}</p>
             </div>
         </div>
     )
