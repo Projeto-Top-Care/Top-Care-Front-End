2b7e959022f0f92b306168337df798bac2539615
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2024-06-20 21:19:42 -0300

    Página inicial está completa!

--- src/components/Botao-categoria-pgInicial/botao-categoria-pgInicial.tsx
+++ src/components/Botao-categoria-pgInicial/botao-categoria-pgInicial.tsx
@@ -11,7 +11,7 @@ const BotaoCategoriaPgInicial = ({ title, image, pagina }: ICategoriaPgInicial) 
     const { push } = useRouter()
 
     return (
-        <div onClick={() => push(pagina)} className="hover:shadow-md hover:translate-y-2 transition duration-200 cursor-pointer lg:w-[17rem] w-full flex flex-col items-center justify-between bg-branco border-2 rounded-xl border-cinza px-2 py-2 sm:py-4 gap-2">
+        <div onClick={() => push(pagina)} className="hover:shadow-md hover:-translate-y-2 transition duration-200 cursor-pointer lg:w-[17rem] w-full flex flex-col items-center justify-between bg-branco border-2 rounded-xl border-cinza px-2 py-2 sm:py-4 gap-2">
             <p className="h-2/5 text-center text-preto font-averia text-base sm:text-lg lg:text-xl font-bold">{title}</p>
             <div className="h-3/5 lg:flex hidden items-center">
                 <img className="w" src={image} />
--- src/components/Lojas/Lojas.tsx
+++ src/components/Lojas/Lojas.tsx
@@ -1,7 +1,7 @@
 import { radio } from "@material-tailwind/react";
 import { LuMapPin } from "react-icons/lu";
 
-interface ILoja{
+interface ILoja {
     src: string,
     cidade: string,
     estado: string,
@@ -11,25 +11,27 @@ interface ILoja{
     funcionamentoHora: string
 }
 
-const Lojas = ({src, cidade, estado, endereco, contato, funcionamentoDias, funcionamentoHora}:ILoja) => {
+const Lojas = ({ src, cidade, estado, endereco, contato, funcionamentoDias, funcionamentoHora }: ILoja) => {
     return (
         <div className="font-poppins w-[300px] hover:scale-105 duration-100">
             <img className=" rounded-t-lg w-full" src={src} />
             <div className="flex justify-center border border-preto border-t-0 rounded-b-lg w-full">
                 <div className="mt-[5%] w-20px h-20px md:mr-3 mr-1">
-                    <LuMapPin size={25} color="#B5A6F3"/>
+                    <a href="https://g.co/kgs/1h7bBNB">
+                        <LuMapPin size={25} color="#B5A6F3" />
+                    </a>
                 </div>
                 <div className="mt-[5%] mb-[10%] w-[78%]">
                     <p className=" text-preto md:text-lg text-sm">Top Care {cidade} - {estado}</p>
                     <div className="text-cinza-escuro md:bg-red">
                         <p className="mt-[10%] font-bold md:text-base text-sm">Endereço</p>
-                        <p className="md:text-sm text-xs">{endereco}</p> 
+                        <p className="md:text-sm text-xs">{endereco}</p>
                         <p className="mt-[10%] font-bold md:text-base text-sm">Contato</p>
                         <p className="md:text-sm text-xs">{contato}</p>
                         <p className="mt-[10%] font-bold md:text-base text-sm">Funcionamento</p>
-                        <p className="md:text-sm text-xs">{funcionamentoDias}</p>    
-                        <p className="md:text-sm text-xs">{funcionamentoHora}</p>  
-                    </div> 
+                        <p className="md:text-sm text-xs">{funcionamentoDias}</p>
+                        <p className="md:text-sm text-xs">{funcionamentoHora}</p>
+                    </div>
                 </div>
             </div>
         </div>
