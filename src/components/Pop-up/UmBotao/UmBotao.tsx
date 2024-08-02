6916b59172c02d2c2ac8c0fc1979348b268b3998
Carlos Bolzanell <carlosbolzanell@gmail.com>
2024-08-02 15:21:39 -0300

    MERGEEEEE

--- src/components/Pop-up/UmBotao/UmBotao.tsx
+++ src/components/Pop-up/UmBotao/UmBotao.tsx
@@ -1,16 +1,23 @@
+import { useRouter } from 'next/navigation'
 import React, { SetStateAction, useEffect, useState } from 'react'
 
 interface IPopUp {
     texto: string,
-    openParms: React.Dispatch<SetStateAction<boolean>>
+    openParms: React.Dispatch<SetStateAction<boolean>>,
+    rota?: string
 }
 
-export default function UmBotao({ texto, openParms}: IPopUp) {
+export default function UmBotao({ texto, openParms, rota}: IPopUp) {
     const [open, setOpen] = useState<boolean>(true)
+    const router = useRouter()
+
     useEffect(()=>{
         openParms(open)
-        if(!open) location.reload();
+        if(!open && rota){
+            router.push(rota)
+        };
     }, [open])
+
     return (
         <div className='h-32 bg-terciaria rounded-lg font-poppins flex items-center justify-center flex-col gap-4'>
             <h2 className='text-xs lg:text-base text-center'>{texto}</h2>
