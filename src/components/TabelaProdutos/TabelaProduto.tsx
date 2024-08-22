45eb61eb78e4dc7698acb7edbe1c473024ba2932
Luana Becker <130504315+LuanaBecker24@users.noreply.github.com>
2024-08-22 18:01:48 -0300

    Arrumando especificação de produtos

--- src/components/TabelaProdutos/TabelaProduto.tsx
+++ src/components/TabelaProdutos/TabelaProduto.tsx
@@ -2,20 +2,22 @@ import { Especificacao, ProdutoCompleto } from '@/types/produto'
 import InputText from "../InputText/InputText";
 import InputFile from '../InputFile/InputFile';
 import TextArea from "../TextArea/TextArea";
-import Select from "../Select/Select";
 import { useState } from "react";
 import { FiPlus } from "react-icons/fi";
+import CadastroEspecificacoes from '../CadastroEspecificacao/CadastroEspecificacao';
+import CardEspecificacaoProduto from '../CardEspecificacaoProduto/CardEspecificacaoProduto';
 
 interface TabelaProdutosProps {
     produtos?: ProdutoCompleto
     produto?: Especificacao
 }
 
-const marcas = ["ZeeDog", "Whiskas", "Royal Canin", "Purina", "Pedigree", "Golden", "TetraMin"]
-
 export default function TabelaProdutos({ produto, produtos }: TabelaProdutosProps) {
-    const [marca, setMarca] = useState("");
     const [quantidadeFotos, setQuantidadeFotos] = useState<number[]>([])
+    const [openEspecificacao, setOpenEspecificacao] = useState<boolean>(false)
+    const [especificacoes, setEspecificacao] = useState<Especificacao[]>(() => {
+        return Array.isArray(produtos?.especificacoes) ? produtos.especificacoes : [];
+    });
 
     return (
         <section className='border border-cinza-escuro rounded-xl h-full flex flex-col lg:flex-row w-full'>
@@ -24,16 +26,8 @@ export default function TabelaProdutos({ produto, produtos }: TabelaProdutosProp
                     <p className='font-averia text-xl font-extrabold md:text-2xl'>Informações básicas</p>
                 </div>
                 <form action="">
-                    <div className=" mt-5" >
-                        <InputText placeholder='Nome do produto*' value={produtos?.nomeProduto} required />
-                    </div>
-                    <div className='mt-5'>
-                        <div className='w-full'>
-                            <InputText placeholder="Código*" value={produtos?.codigo} required />
-                        </div>
-                    </div>
-                    <div className='mt-5'>
-                        <Select label="Marca" options={marcas} opcaoSelecionada={setMarca} opcao={marca} />
+                    <div className="mt-5">
+                        <InputText placeholder='Nome do produto*' value={produto?.nome} required />
                     </div>
                     <div className='mt-5 h-32 mb-8'>
                         <TextArea placeholder='Descrição' value={produtos?.descricao} />
@@ -44,31 +38,24 @@ export default function TabelaProdutos({ produto, produtos }: TabelaProdutosProp
                 <div className="flex justify-center lg:block pt-6">
                     <p className="font-averia text-xl font-extrabold md:text-2xl">Especificações</p>
                 </div>
-                <form action="">
-                    <div className="flex flex-col w-full gap-5">
-                        <div className=" flex flex-col md:flex-row justify-between mt-5 w-full gap-5">
-                            <div className="w-full">
-                                <InputText placeholder='Idade do pet*' value={produto?.idadePet} />
-                            </div>
-                            <div className="w-full">
-                                <InputText placeholder='Porte de raça*' value={produto?.porteRaca} />
-                            </div>
+                <div className='flex flex-col w-full gap-4 mt-4 mb-8'>
+                    {especificacoes.map((especificacao) => (
+                        <CardEspecificacaoProduto descricao={especificacao.descricao} nome={especificacao.nome} />
+                    ))}
+                    <div className='flex flex-row gap-2 cursor-pointer md:w-[10%] w-full mt-4' onClick={() => setOpenEspecificacao(true)}>
+                        <div className='p-2 rounded-full bg-terciaria'>
+                            <FiPlus size={20} />
                         </div>
-                        <div className="flex flex-col gap-5 md:flex-row justify-between w-full">
-                            <div className="w-full">
-                                <InputText placeholder='Categoria*' value={produto?.tipo} />
-                            </div>
-                            <div className="w-full">
-                                <InputText placeholder='Material*' value={produto?.material} />
-                            </div>
-                        </div>
+                        <p className='font-poppins text-sm text-preto w-full'>
+                            Adicionar especificação
+                        </p>
                     </div>
-                    <div className="flex flex-col lg:flex-row">
-                        <div className="w-full mt-5">
-                            <InputText placeholder='Pet(s)*' value={produto?.pet} />
-                        </div>
-                    </div>
-                </form>
+                </div>
+                {
+                    openEspecificacao && (
+                        <CadastroEspecificacoes openModalProps={setOpenEspecificacao} especificacoesProps={especificacoes} setEspecificacaoProps={setEspecificacao} />
+                    )
+                }
                 <div className='flex flex-col items-center mt-5'>
                     <h1 className='font-averia font-extrabold text-lg text-center'>Imagem</h1>
                     <p className='font-poppins text-center text-sm'>Para adicionar mais fotos aperte no sinal de mais</p>
@@ -80,20 +67,18 @@ export default function TabelaProdutos({ produto, produtos }: TabelaProdutosProp
                                 </div>
                                 <p className='font-poppins text-xs md:text-sm text-center mt-1'>Principal</p>
                             </div>
-                            {
-                                quantidadeFotos.map((number, i) => (
-                                    <div className='flex flex-col items-center animate-checked' key={i}>
-                                        <div className='w-20 h-20 md:w-24 md:h-24 lg:w-20 lg:h-20 xl:w-24 xl:h-24'>
-                                            <InputFile rounded='rounded-lg' />
-                                        </div>
-                                        <p className='font-poppins text-xs md:text-sm text-center mt-1'>Imagem {number + 1}</p>
+                            {quantidadeFotos.map((number, i) => (
+                                <div className='flex flex-col items-center animate-checked' key={i}>
+                                    <div className='w-20 h-20 md:w-24 md:h-24 lg:w-20 lg:h-20 xl:w-24 xl:h-24'>
+                                        <InputFile rounded='rounded-lg' />
                                     </div>
-                                ))
-                            }
+                                    <p className='font-poppins text-xs md:text-sm text-center mt-1'>Imagem {number + 1}</p>
+                                </div>
+                            ))}
                         </div>
                         <div className={`${quantidadeFotos.length < 4 ? '!flex' : 'hidden'} items-center justify-center cursor-pointer w-full md:w-[10%] mt-[-25%] md:mt-[-5%]`}>
                             <div className='p-3 rounded-full bg-terciaria cursor-pointer' onClick={() => setQuantidadeFotos([...quantidadeFotos, quantidadeFotos.length + 1])}>
-                                {<FiPlus size={20} />}
+                                <FiPlus size={20} />
                             </div>
                         </div>
                     </div>
